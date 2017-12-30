6417335747c92ffd37f835eea6e0ccf8d996f30c
willardshikami <willardshikami@gmail.com>
2017-12-30 16:47:43 +0300

    added http providers to main module & corrected api endpoint

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -6,12 +6,21 @@ import { SpotifyService } from '../../services/spotify.services';
   moduleId: module.id,
   selector: 'search',
   templateUrl: 'search.component.html',
+  providers: [ SpotifyService ]
 })
 export class SearchComponent  {
-  searchStr: String;
+  searchStr: string;
+
+  //inject spotify service
+  constructor(private _spotifyService: SpotifyService){
+
+  }
   
     //Initialize the search music function
     searchMusic(){
-      console.log(this.searchStr);
+      this._spotifyService.searchMusic(this.searchStr)
+      .subscribe( res => {
+        console.log(res.artists.items);
+      })
     }
 } 
--- src/app/services/spotify.services.ts
+++ src/app/services/spotify.services.ts
@@ -11,7 +11,7 @@ export class SpotifyService{
   }
 
   searchMusic(str:string, type='artist'){
-    this.searchUrl = 'https://api.spotify.com/search?query='+str+'&ooffset=0&limit=20&type='+type+'&market=US';
+    this.searchUrl = 'https://api.spotify.com/v1/search?query='+str+'&ooffset=0&limit=20&type='+type+'&market=US';
     return this._http.get(this.searchUrl)
     .map( res => res.json() );
   }
